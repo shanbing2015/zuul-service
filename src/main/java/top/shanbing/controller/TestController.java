2d93d2a842d94f2e0c4d1c748058050ee1489472
shanbing <shanbing@seeapp.com>
2018-07-02 10:11:15 +0800

    test

---
+++ src/main/java/top/shanbing/controller/TestController.java
@@ -0,0 +1,19 @@
+package top.shanbing.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        String datetime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
+        System.out.println(datetime);
+        return datetime;
+    }
+}
